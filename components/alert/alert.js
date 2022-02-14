6b92de0baa8f2bab4475a1789362ea2918b8765f
Roshan J <devenforced@gmail.com>
2022-02-14 23:47:38 +0530

    Added basic layout related to documentation and home page setup

--- components/alert/alert.js
+++
@@ -1,6 +0,0 @@
-const closeIcon = document.querySelectorAll(".close-icon")
-closeIcon.forEach((eachIcon) => {
-    eachIcon.addEventListener("click", (e) => {
-        e.target.parentElement.style.display = "none";
-    })
-})
